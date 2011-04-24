7eac09cc5e5ce41eeede5e02604054a14045d3a1
Suresh Iyer <protoiyer@gmail.com>
2011-04-25 00:34:23 +0530

    Task C: Catalog display done (including Playtime exercises)

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -15,6 +15,10 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '.list_line_odd', :minimum => 1
+    assert_select '.list_line_even', :minimum => 1
+    assert_select 'dt', 'Programming Ruby 1.9'
+    assert_select 'dt', 3
   end
 
   test "should get new" do
