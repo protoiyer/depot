37d25c0d4278ee05d52d3a1bf95ad12beeb4a8b4
Suresh Iyer <protoiyer@gmail.com>
2011-04-27 17:38:07 +0530

    Task D and E done, sans E Playtime

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,11 @@ class StoreController < ApplicationController
   def index
     @products = Product.all
     @time = Time.now
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+    @counter = session[:counter]
   end
 
 end
